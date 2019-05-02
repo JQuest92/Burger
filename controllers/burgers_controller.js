9167a389e9c897913293f1046ae204a799466977
John Westbrooks <johnwestbrooks@Johns-MacBook-Pro-4.local>
2019-05-02 14:55:33 -0500

    Finished views, burgers.js,and all config files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,42 +3,55 @@ var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger.js');
 
-
-router.get('/', function(req, res)
- {
-    res.redirect('/burgers');
-});
-
-router.get('/burgers', function(req, res) {
-    burger.all(function(burger_data)
-     {
-        res.render('index', { burger_data });
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
     });
-});
-
-
-router.post('/burgers/create', function(req, res) {
-    if (req.body.burger_name == '') 
-    {
-        console.log('No burger name entered');
-        res.redirect('/');
-    } 
-    else 
-    {
-        burger.create(req.body.burger_name, function(result) {
-            console.log(result);
-            res.redirect('/');
-        });
-    }
-});
-
-router.put('/burgers/update', function(req, res)
- {
-    burger.update(req.body.burger_id, function(result)
-     {
-        console.log(result);
-        res.redirect('/');
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    cat.insertOne([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
     });
-});
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.updateOne({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
+  
 
-module.exports = router;
+
+
+
+
+
+
+
+
+
